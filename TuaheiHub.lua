2fd96fd22cd93443ca86b67c9fbb2f038d2f3ffa
TuaheiHubLink <khabxs361@Gmail.com>
2025-02-11 23:03:05 +0700

    Update TuaheiHub.lua

--- TuaheiHub.lua
+++ TuaheiHub.lua
@@ -1,52 +1,92 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Tuahei Hub", "Ocean")
+--[[
+    Open Src AutoFisch Script By : NOOBHUB
+]]
+local Player = game:GetService("Players")
+local LocalPlayer = Player.LocalPlayer
+local Char = LocalPlayer.Character
+local Humanoid = Char.Humanoid
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GuiService = game:GetService("GuiService")
 
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Main Player")
-
-Section:NewButton("noclip", "!!!", function()
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-
-local noclipEnabled = true -- เปิด noclip ทันทีเมื่อเริ่มต้น
-local player = Players.LocalPlayer
-
-local function toggleNoclip()
-    noclipEnabled = not noclipEnabled
-
-    if noclipEnabled then
-        print("Noclip Enabled")
-    else
-        print("Noclip Disabled")
+equipitem = function(v)
+if LocalPlayer.Backpack:FindFirstChild(v) then
+    local a = LocalPlayer.Backpack:FindFirstChild(v)
+        Humanoid:EquipTool(a)
     end
 end
 
--- อัปเดตสถานะการเดินทะลุเมื่อ noclip เปิดอยู่
-RunService.Stepped:Connect(function()
-    if noclipEnabled and player.Character then
-        for _, part in pairs(player.Character:GetDescendants()) do
-            if part:IsA("BasePart") then
-                part.CanCollide = false
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Tuahri Hub", "BloodTheme")
+local Tab = Window:NewTab("Menu")
+local Section = Tab:NewSection("MAIN")
+
+-- AutoCast
+Section:NewToggle("AutoCast", "", function(v)
+_G.AutoCast = v
+     pcall(function()
+while _G.AutoCast do wait()
+    local Rod = Char:FindFirstChildOfClass("Tool")
+                task.wait(.1)
+                    Rod.events.cast:FireServer(100,1)
+        end
+    end)
+end)
+
+Section:NewToggle("AutoShake", "", function(v)
+    _G.AutoShake = v
+pcall(function()
+while _G.AutoShake do wait()
+              task.wait(0.01)
+                local PlayerGUI = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+                local shakeUI = PlayerGUI:FindFirstChild("shakeui")
+                if shakeUI and shakeUI.Enabled then
+                    local safezone = shakeUI:FindFirstChild("safezone")
+                    if safezone then
+                        local button = safezone:FindFirstChild("button")
+                        if button and button:IsA("ImageButton") and button.Visible then
+                                GuiService.SelectedObject = button
+                                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+                    end
+                end
             end
         end
-    elseif not noclipEnabled and player.Character then
-        for _, part in pairs(player.Character:GetDescendants()) do
-            if part:IsA("BasePart") then
-                part.CanCollide = true
+    end)
+end)
+-- AutoReel
+Section:NewToggle("AutoReel", "", function(v)
+     _G.AutoReel = v
+pcall(function()
+    while _G.AutoReel do wait()
+            for i,v in pairs(LocalPlayer.PlayerGui:GetChildren()) do
+                if v:IsA "ScreenGui" and v.Name == "reel"then
+                    if v:FindFirstChild "bar" then
+                        wait(.15)
+                            ReplicatedStorage.events.reelfinished:FireServer(100,true)
+                    end
+                end
             end
+        end
+    end)
+end)
+
+Section:NewToggle("Freeze Character", "", function(v)
+    Char.HumanoidRootPart.Anchored = v
+end)
+
+-- equipitem
+spawn(function()
+    while wait() do
+        if _G.AutoCast then
+            pcall(function()
+                for i,v in pairs(LocalPlayer.Backpack:GetChildren()) do
+                    if v:IsA ("Tool") and v.Name:lower():find("rod") then
+                    equipitem(v.Name)
+                    end
+                end
+            end)
         end
     end
-end)
-
-end)
-
-Section:NewButton("fly", "!!!", function()
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
-end)
---------------ด้านบนMain
-
-local Tab = Window:NewTab("Farm")
-local Section = Tab:NewSection("Farming")
-
-Section:NewButton("auto ", "!!!", function()
 end)
